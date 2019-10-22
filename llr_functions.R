1e4e90dc2a7f6223ca3fa3a8edf41b838aa57aea
Corey Maxedon <cmaxedon15@gmail.com>
2019-10-22 18:26:31 -0400

    update bench and llr

--- llr_functions.R
+++ llr_functions.R
@@ -56,27 +56,6 @@ make_predictor_matrix = function(x) {
 }
 
 
-# --- example 1 --- #
-
-# get the data
-data(french_fries, package = 'reshape2')
-french_fries = na.omit(french_fries)
-
-# input data
-x = french_fries$potato
-y = french_fries$buttery
-
-# space along which to smooth
-z = seq(0, 15, length.out = 100)
-
-# run smoothing
-fits = llr(z = z, x = x, y = y, omega = 2)
-
-# plot the data and the smoother
-plot(x, y)
-lines(z, fits, col = 'red')
-
-
 # --- example 2 --- #
 
 # noisy sine wave
