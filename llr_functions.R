3aac7393b5e42cb44b5590d70c4086f7e8d0eca7
Corey Maxedon <cmaxedon15@gmail.com>
2019-10-22 22:25:44 -0400

    edit llr

--- llr_functions.R
+++ llr_functions.R
@@ -23,7 +23,6 @@ llr = function(x, y, z, omega) {
 compute_f_hat = function(z, x, y, omega) {
   Wz = diag(make_weight_matrix(z, x, omega))
   X = make_predictor_matrix(x)
-  #f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
   f_hat = c(1, z) %*% solve(t(X) %*% apply(Wz, 1, function(butt){butt * X})) %*% t(X) %*% apply(Wz, 1, function(butt){butt * y})
   return(f_hat)
 }
