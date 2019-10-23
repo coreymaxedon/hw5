e5a861d8c3a0e78756e319b233c3ed9a78862c2f
Corey Maxedon <cmaxedon15@gmail.com>
2019-10-22 22:18:53 -0400

    edit benchmark

--- benchmark_llr.R
+++ benchmark_llr.R
@@ -1,8 +1,3 @@
 library(microbenchmark)
 
 microbenchmark(source("llr_functions.R"), times = 10)
-
-
-Unit: milliseconds
-expr      min       lq     mean   median       uq      max neval
-source("llr_functions.R") 493.8058 642.8336 788.2934 713.6931 915.1111 1347.226   100
